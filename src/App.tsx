6af560bbf94044df792df1cab08748b37ac762f4
Rieu Thomas <rieu.thomas@hotmail.com>
2025-12-29 21:49:11 +0100

    Initialisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/reset.css'
+import './styles/App.css'
+import { Link, Outlet } from 'react-router'
+import { useEffect, useState } from "react"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <header>
+        <Link to="/">
+          <h1>Thomas RIEU</h1>
+        </Link>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          {time.getHours().toString().padStart(2, "0")}:{time.getMinutes().toString().padStart(2, "0")}
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </header>
+      
+      <main>
+        <Outlet />
+      </main>
+
+      <footer>
+        <a href="https://github.com/RieuThomas" target='_blank'>
+          <img src="/github.png" alt="github" />
+        </a>
+        <a href="https://www.linkedin.com/in/thomas-rieu/" target='_blank'>
+          <img src="/linkedin.png" alt="linkedin" />
+        </a>
+        <a href="mailto:rieu.thomas@hotmail.com"target='_blank'>
+          <img src="mail.png" alt="mail" />
+        </a>
+      </footer>
     </>
   )
 }
