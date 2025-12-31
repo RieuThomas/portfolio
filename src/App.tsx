eb3a695bbb779ce4dd483b7931de9624983c601b
Rieu Thomas <rieu.thomas@hotmail.com>
2025-12-31 15:12:11 +0100

    Update App.tsx
    Delete Time header and footer adding new format with aside and a navbar in main

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,41 @@
 import './styles/reset.css'
 import './styles/App.css'
 import { Link, Outlet } from 'react-router'
-import { useEffect, useState } from "react"
 
 function App() {
-  const [time, setTime] = useState(new Date())
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date())
-    }, 1000)
-
-    return () => clearInterval(interval)
-  }, [])
 
   return (
-    <>
-      <header>
-        <Link to="/">
-          <h1>Thomas RIEU</h1>
-        </Link>
-        <p>
-          {time.getHours().toString().padStart(2, "0")}:{time.getMinutes().toString().padStart(2, "0")}
-        </p>
-      </header>
-      
+    <>      
       <main>
-        <Outlet />
+        <aside className='sidebar'>  
+          <div className='sidebar-infos'>
+            <img src="/avatar.png" alt="" />
+            <h2>Thomas RIEU</h2>
+            <p>Développeur Full-Stack</p>
+          </div>
+          <div className='sidebar-details'>
+            <a href="https://github.com/RieuThomas" target='_blank'>
+              <img src="/github.png" alt="github" />
+            </a>
+            <a href="https://www.linkedin.com/in/thomas-rieu/" target='_blank'>
+              <img src="/linkedin.png" alt="linkedin" />
+            </a>
+            <a href="mailto:rieu.thomas@hotmail.com"target='_blank'>
+              <img src="mail.png" alt="mail" />
+            </a>     
+          </div>
+        </aside>
+        <section className='main-section'>
+          <ul className='navbar'>
+            <li><Link to="/">A propos</Link></li>
+            <li><Link to="/project">Projets</Link></li>
+            <li><Link to="/skill">Skills</Link></li>
+          </ul>
+          <div className='outlet-section'>
+            <Outlet />
+          </div>
+        </section>
       </main>
-
-      <footer>
-        <a href="https://github.com/RieuThomas" target='_blank'>
-          <img src="/github.png" alt="github" />
-        </a>
-        <a href="https://www.linkedin.com/in/thomas-rieu/" target='_blank'>
-          <img src="/linkedin.png" alt="linkedin" />
-        </a>
-        <a href="mailto:rieu.thomas@hotmail.com"target='_blank'>
-          <img src="mail.png" alt="mail" />
-        </a>
-      </footer>
     </>
   )
 }
