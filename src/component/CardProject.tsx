e37c31f8a4cb543dfcc95e56ef640d0bec00c373
Rieu Thomas <rieu.thomas@hotmail.com>
2025-12-31 15:10:41 +0100

    Update CardProject.tsx

--- src/component/CardProject.tsx
+++ src/component/CardProject.tsx
@@ -1,3 +1,6 @@
+import "../styles/CardProject.css"
+import { useState } from "react";
+
 interface CardProjectProps {
     title: string
     image: string
@@ -5,15 +8,33 @@ interface CardProjectProps {
     webLink?: string
 }
 
-function CardProject({project}: {project:CardProjectProps}) {
+function CardProject({ project }: { project: CardProjectProps }) {
+    const [showLinkAndTitle, setShowLinkAndTitle] = useState(false);
+
     return (
-        <article>
+        <article
+            className={`card-project ${showLinkAndTitle ? "is-hovered" : ""}`}
+            onMouseEnter={() => setShowLinkAndTitle(true)}
+            onMouseLeave={() => setShowLinkAndTitle(false)}
+        >
             <img src={project.image} alt={project.title} />
-            <h3>{project.title}</h3>
-            <a href={project.gitHub} target="_blank">Repo Github</a>
-            <a href={project.webLink} target="_blank">Viste site web</a>
+
+            <div className="written-overlay">
+                <h3>{project.title}</h3>
+                <div className="link">
+                    <a href={project.gitHub} target="_blank">
+                        Repo Github
+                    </a>
+                    {project.webLink &&
+                        <a href={project.webLink} target="_blank">
+                            Site Web
+                        </a>
+                    }
+
+                </div>
+            </div>
         </article>
-    )
+    );
 }
 
 export default CardProject
