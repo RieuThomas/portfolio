63d2dd7047036665bbdf2124e24a77c730abd0a8
Rieu Thomas <rieu.thomas@hotmail.com>
2026-01-04 22:02:18 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,24 @@
-import { Link } from "react-router"
 import "../styles/Home.css"
 
 function Home() {
     return (
-        <section className="home">
-             <Link to="/project">
-               <article>
-                 <h2>Project</h2>
-                 <img src="/site-internet.png" alt="web" />
-               </article>
-             </Link>
-             <Link to="/about">
-               <article>
-                  <h2>A propos</h2>
-               </article>
-             </Link>
-             <Link to="/project">
-               <article>
-                  <h2>Project</h2>
-               </article>
-             </Link>
-        </section>
-    )    
-    
-    
+        <>
+        <h1>A propos</h1>
+          <p className="about">
+            Développeur web full-stack en formation, avec une forte appétence pour le front-end, je conçois des interfaces modernes, performantes et bien structurées.<br /><br />
+
+            Issu d’une formation scientifique, j’aborde le développement avec rigueur, méthode et sens de l’analyse, en accordant une attention particulière à la qualité du code et à l’expérience utilisateur.<br /><br />
+
+            Dans le cadre de ma formation, je développe des applications avec React, TypeScript et JavaScript côté front-end, et je découvre le back-end avec Node.js, Express et MySQL, en m’intéressant à la structuration des données et aux échanges via API.<br /><br />
+
+            Curieux et passionné par la création de produits interactifs, je m’inspire aussi de l’univers du jeu vidéo, notamment pour la conception d’expériences utilisateur claires et engageantes.<br /><br />
+
+            Actuellement à la recherche d’une alternance pour un titre Concepteur Développeur d’Applications (CDA) à partir de mars 2026, je souhaite rejoindre une équipe où je pourrai continuer à apprendre et contribuer à des projets concrets. <br /><br />
+          </p>
+        </>
+          
+    )
 }
+
 
 export default Home
