05742f0e3cf53975e8ae3e9cd0a505ae8deef5db
Rieu Thomas <rieu.thomas@hotmail.com>
2026-01-04 22:00:30 +0100

    Update of CardProject
    Modification of the logic of the hover for the responsive + css file

--- src/component/CardProject.tsx
+++ src/component/CardProject.tsx
@@ -6,31 +6,37 @@ interface CardProjectProps {
     image: string
     gitHub: string
     webLink?: string
+    tag: string
 }
 
 function CardProject({ project }: { project: CardProjectProps }) {
-    const [showLinkAndTitle, setShowLinkAndTitle] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const isMobile = window.matchMedia("(hover: none)").matches;
 
+    const handleCardClick = () => {
+        if (isMobile) {
+            setIsOpen(prev => !prev);
+        }
+    };
+  
     return (
         <article
-            className={`card-project ${showLinkAndTitle ? "is-hovered" : ""}`}
-            onMouseEnter={() => setShowLinkAndTitle(true)}
-            onMouseLeave={() => setShowLinkAndTitle(false)}
+            className={`card-project ${isOpen ? "is-open" : ""}`}
+            onClick={handleCardClick}
         >
+            <span className={project.tag}>{project.tag}</span>
             <img src={project.image} alt={project.title} />
-
             <div className="written-overlay">
                 <h3>{project.title}</h3>
                 <div className="link">
-                    <a href={project.gitHub} target="_blank">
-                        Repo Github
+                <a href={project.gitHub} target="_blank">
+                    Repo Github
+                </a>
+                {project.webLink && (
+                    <a href={project.webLink} target="_blank">
+                    Site Web
                     </a>
-                    {project.webLink &&
-                        <a href={project.webLink} target="_blank">
-                            Site Web
-                        </a>
-                    }
-
+                )}
                 </div>
             </div>
         </article>
