154e51ae119c6c2abf809921fc7c12c95afee0a6
Rieu Thomas <rieu.thomas@hotmail.com>
2026-01-05 16:09:07 +0100

    Update CardIcon.tsx

--- src/component/CardIcon.tsx
+++ src/component/CardIcon.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import "../styles/CardIcon.css"
 
 interface CardIconProps {
@@ -8,10 +9,16 @@ interface CardIconProps {
 }
 
 function CardIcon({ icon }: { icon: CardIconProps }) {
+  const [isTitileVisible, setIsTitleVisible] = useState(false)
+  const isMobile = window.matchMedia("(hover: none)").matches;
+
   return (
     <div className="card-skill">
-      <h3 className="icon-title">{icon.name}</h3>
-      <article className={`skill ${icon.name}`}>
+      {isMobile ? <h3 className="icon-title">{icon.name}</h3> : !icon.link ? <h3 className="icon-title">{icon.name}</h3>
+      : isTitileVisible && <h3 className="icon-title">{icon.name}</h3> }
+      <article className={`skill ${icon.name}`}
+        onMouseEnter={() => setIsTitleVisible(true)}
+        onMouseLeave={() => setIsTitleVisible(false)}>
         <img src={icon.image} alt={icon.name} />
       </article>
     </div>
